d21f1097d1037603a9d146ad74ec887040d0cb2b
Florencia-Herrera <herreraflorencia874@gmail.com>
2025-10-21 16:28:13 -0300

    Create Celular.java

---
+++ TP5_RelacionesUML/Celular.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo.práctico.pkg5;
+
+public class Celular {
+    private String imei;
+    private String marca;
+    private String modelo;
+    private Bateria bateria;
+    private Usuario usuario;
+
+    public Celular(String imei, String marca, String modelo, Bateria bateria) {
+        this.imei = imei;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.bateria = bateria;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        usuario.setCelular(this); 
+    }
+
+    public String getImei() { return imei; }
+    public String getMarca() { return marca; }
+    public String getModelo() { return modelo; }
+    public Bateria getBateria() { return bateria; }
+    public Usuario getUsuario() { return usuario; }
+
+    @Override
+    public String toString() {
+        return "Celular [imei=" + imei + ", marca=" + marca + ", modelo=" + modelo +
+               ", bateria=" + bateria + ", usuario=" + usuario + "]";
+    }   
+}
